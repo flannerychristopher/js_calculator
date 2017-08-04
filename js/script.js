66789c440904f31b61d7c9e3d60dc63b4ac5ad02
chris flan <flanthedev@gmail.com>
2017-08-04 16:13:17 +0800

    fix math functions and add click event listeners

--- js/script.js
+++ js/script.js
@@ -1,14 +1,44 @@
+function multiply(a, b) {
+	let answer = parseInt(a) * parseInt(b);
+  return answer;
+}
+function divide(a, b) {
+	parseInt(a) / parseInt(b);
+	return answer;
+}
 function add(a, b) {
-	let answer = parseInt(a) + parseInt(b);
+  let answer = parseInt(a) + parseInt(b);
   return answer;
 }
 function subtract(a, b) {
   let answer = parseInt(a) - parseInt(b);
   return answer;
 }
-function divide(a, b) {
-	parseInt(a) / parseInt(b)
-}
-function multiply(a, b) {
-	parseInt(a) * parseInt(b)
+
+const equation = document.getElementById('equation');
+const solution = document.querySelector('div#solution');
+const buttons = document.getElementsByTagName('button');
+let equationArray;
+
+for (let i = 0; i < buttons.length; i++) {
+  const button = buttons[i];
+  button.addEventListener("click", function() {
+    if (button.textContent === 'C/AC' ) {
+      equation.value = '';
+      return;
+    } else {
+      if (button.textContent === '=' && equation.textContent.indexOf('+') > -1) {
+        equationArray = equation.value.split('+');
+        printAnswer( add(equationArray[0], equationArray[1]) );
+        return;
+      } else if (button.textContent === '=' && equation.textContent.indexOf('-') > -1) {
+        equationArray = equation.value.split('-');
+        printAnswer( subtract(equationArray[0], equationArray[1]) );
+        return;
+      } else {
+        equation.textContent += button.textContent;
+        return;
+      }
+    }
+  });
 }
