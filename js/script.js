4696bcebb0f8b943dc00c77ea3f3e1912f107d4e
chris flan <flanthedev@gmail.com>
2017-08-04 15:07:34 +0800

    basic math functions

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+	let answer = parseInt(a) + parseInt(b);
+  return answer;
+}
+function subtract(a, b) {
+  let answer = parseInt(a) - parseInt(b);
+  return answer;
+}
+function divide(a, b) {
+	parseInt(a) / parseInt(b)
+}
+function multiply(a, b) {
+	parseInt(a) * parseInt(b)
+}
