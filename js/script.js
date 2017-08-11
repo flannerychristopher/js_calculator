0c6215c882ece2c1eb48c65c85f70a4616f41447
flan the dev <flanthedev@gmail.com>
2017-08-11 20:57:50 +0800

    refactor calc.multiply

--- js/script.js
+++ js/script.js
@@ -1,48 +1,29 @@
-let calculator = {
-  sum: 0,
-  difference: 0,
-  product: 0,
-  quotient: 0,
+const calc = {
+  // sum: 0,
+  // difference: 0,
+  // product: 1,
+  // quotient: 1,
+  //
+  // reset: () => {
+  //   calc.sum = 0;
+  //   calc.different = 0;
+  //   calc.product = 1;
+  //   calc.quotient = 1;
+  // },
 
-  reset: () => {
-    console.log(this.sum);
-    this.sum = 0;
-    this.different = 0;
-    this.product = 0;
-    this.quotient = 0;
+  run: (array) => {
+    // calc.reset();
+    calc.multiply(array);
+    console.log(array);
   },
 
-  calculate: (array) => {
-    calculator.reset;
-    // this.multiply(array);
-    console.log(this.product);
-  },
-
-  multiply: (equation) => {
-
-    for (i = 0; i < equation.length; i++) {
-      let previous = equation.indexOf(equation[i-1]);
-      let current = equation.indexOf(equation[i]);
-      let next = equation.indexOf(equation[i+1]);
-
-      console.log(previous);
-
-      if ( equation[i] == 'x' ) {
-        if ( previous !== -1 ) {
-          let answer = parseInt(equation[i-1]) * parseInt(equation[i+1]);
-          this.product *= answer;
-          equation.splice(previous, 3);
-
-          console.log(equation);
-          if (equation.length) this.multiply(equation);
-        } else if ( previous === -1 ) {
-          let answer = parseInt(equation[i+1]);
-          this.product *= answer;
-          console.log(current);
-          equation.splice(current, 2);
-          console.log(equation);
-          if (equation.length) this.multiply(equation);
-        }
+  multiply: (array) => {
+    for (i = 0; i < array.length; i++) {
+      if ( array[i] == 'x' && previous != -1 ) {
+        let answer = parseInt(array[i-1]) * parseInt(array[i+1]);
+        array.splice( array.indexOf(array[i-1]), 3, answer );
+        console.log(array);
+        if (array.length) calc.multiply(array);
       }
     }
   },
