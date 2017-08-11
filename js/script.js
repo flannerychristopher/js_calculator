f89d5689283bada78de210524453cd0c019fb910
flan the dev <flanthedev@gmail.com>
2017-08-11 21:17:21 +0800

    add and subtract functions

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const calc = {
   run: (array) => {
     calc.multiply(array);
     calc.divide(array);
+    calc.add(array);
+    calc.subtract(array);
     console.log(array);
   },
 
@@ -23,6 +25,28 @@ const calc = {
         array.splice( array.indexOf( array[i-1]), 3, answer );
         console.log(array);
         if ( array.length ) calc.divide(array);
+      }
+    }
+  },
+
+  add: (array) => {
+    for ( let i = 0; i < array.length; i++ ) {
+      if (array[i] === '+' ) {
+        let answer = parseInt(array[i-1]) + parseInt(array[i+1]);
+        array.splice( array.indexOf( array[i-1]), 3, answer );
+        console.log(array);
+        if ( array.length ) calc.add(array);
+      }
+    }
+  },
+
+  subtract: (array) => {
+    for (let i = 0; i < array.length; i++ ) {
+      if (array[i] === '-') {
+        let answer = parseInt(array[i-1] - parseInt(array[i+1]));
+        array.splice( array.indexOf( array[i-1]), 3, answer );
+        console.log(array);
+        if ( array.length ) calc.subtract(array);
       }
     }
   }
