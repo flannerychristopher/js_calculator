9ab0ac5c248469c6899b9b19ac3434520558db87
flan the dev <flanthedev@gmail.com>
2017-08-11 20:33:46 +0800

    create calc object

--- js/script.js
+++ js/script.js
@@ -1,106 +1,50 @@
-const equation = document.getElementById('equation');
-const solution = document.querySelector('div#solution');
-const buttons = document.getElementsByTagName('button');
-let equationArray;
+let calculator = {
+  sum: 0,
+  difference: 0,
+  product: 0,
+  quotient: 0,
 
-function multiply(a, b) {
-	let answer = parseInt(a) * parseInt(b);
-  return answer;
-}
-function divide(a, b) {
-	parseInt(a) / parseInt(b);
-	return answer;
-}
-function add(a, b) {
-  let answer = parseInt(a) + parseInt(b);
-  return answer;
-}
-function subtract(a, b) {
-  let answer = parseInt(a) - parseInt(b);
-  return answer;
-}
+  reset: () => {
+    console.log(this.sum);
+    this.sum = 0;
+    this.different = 0;
+    this.product = 0;
+    this.quotient = 0;
+  },
 
-function reset() { // reset values after a calculation
-  sum = 0;
-  difference = 0;
-  product = 1;
-  quotient = 1;
-}
+  calculate: (array) => {
+    calculator.reset;
+    // this.multiply(array);
+    console.log(this.product);
+  },
 
-function setIndexRef(equation) { // set the index references for loop
-  let previous = equation.indexOf(equation[i-1]);
-  let current = equation.indexOf(equation[i]);
-  let next = equation.indexOf(equation[i+1]);
-}
+  multiply: (equation) => {
 
+    for (i = 0; i < equation.length; i++) {
+      let previous = equation.indexOf(equation[i-1]);
+      let current = equation.indexOf(equation[i]);
+      let next = equation.indexOf(equation[i+1]);
 
-for (let i = 0; i < buttons.length; i++) {
-  const button = buttons[i];
-  button.addEventListener("click", function() {
-    if (button.textContent === 'C/AC' ) {
-      equation.value = '';
-      return;
-    } else {
-      if (button.textContent === '=' && equation.textContent.indexOf('+') > -1) {
-        equationArray = equation.value.split('+');
-        printAnswer( add(equationArray[0], equationArray[1]) );
-        return;
-      } else if (button.textContent === '=' && equation.textContent.indexOf('-') > -1) {
-        equationArray = equation.value.split('-');
-        printAnswer( subtract(equationArray[0], equationArray[1]) );
-        return;
-      } else {
-        equation.textContent += button.textContent;
-        return;
+      console.log(previous);
+
+      if ( equation[i] == 'x' ) {
+        if ( previous !== -1 ) {
+          let answer = parseInt(equation[i-1]) * parseInt(equation[i+1]);
+          this.product *= answer;
+          equation.splice(previous, 3);
+
+          console.log(equation);
+          if (equation.length) this.multiply(equation);
+        } else if ( previous === -1 ) {
+          let answer = parseInt(equation[i+1]);
+          this.product *= answer;
+          console.log(current);
+          equation.splice(current, 2);
+          console.log(equation);
+          if (equation.length) this.multiply(equation);
+        }
       }
     }
-  });
-}
+  },
 
-function calculator(equation) {
-  for (i = 0; i < equation.length; i++) {
-
-    let previous = equation.indexOf(equation[i-1]);
-    let current = equation.indexOf(equation[i]);
-    let next = equation.indexOf(equation[i+1]);
-
-    if ( equation[i] === 'x' ) {
-      if ( previous === -1 ) {
-        product *= parseInt(equation[i+1]);
-        equation.splice(current, 2);
-        calculate(equation);
-      } else {
-        let answer = multiply(equation[i-1], equation[i+1]);
-        product *= answer;
-        equation.splice(previous, 3);
-        calculate(equation);
-      }
-
-    } else if ( equation[i] === '+' ) {
-      if ( previous === -1 ) {
-        sum += parseInt(equation[i+1]);
-        equation.splice(current, 2);
-        calculate(equation);
-      } else {
-        let answer = add(equation[i-1], equation[i+1]);
-        sum += answer;
-        equation.splice(previous, 3);
-        calculate(equation);
-      }
-
-    } else if (equation[i] === '-') {
-      if ( previous === -1 ) {
-        difference -= parseInt(equation[i+1]);
-        equation.splice(current, 2);
-        calculate(equation);
-      } else {
-        let answer = subtract(equation[i-1], equation[i+1]);
-        difference += answer;
-        equation.splice(previous, 3);
-        calculate(equation);
-      }
-    }
-  }
-  let solution = (product / quotient) + sum + difference;
-  return solution;
 }
---
+++ js/script2.js
@@ -0,0 +1,106 @@
+const equation = document.getElementById('equation');
+const solution = document.querySelector('div#solution');
+const buttons = document.getElementsByTagName('button');
+let equationArray;
+
+function multiply(a, b) {
+	let answer = parseInt(a) * parseInt(b);
+  return answer;
+}
+function divide(a, b) {
+	parseInt(a) / parseInt(b);
+	return answer;
+}
+function add(a, b) {
+  let answer = parseInt(a) + parseInt(b);
+  return answer;
+}
+function subtract(a, b) {
+  let answer = parseInt(a) - parseInt(b);
+  return answer;
+}
+
+function reset() { // reset values after a calculation
+  sum = 0;
+  difference = 0;
+  product = 1;
+  quotient = 1;
+}
+
+function setIndexRef(equation) { // set the index references for loop
+  let previous = equation.indexOf(equation[i-1]);
+  let current = equation.indexOf(equation[i]);
+  let next = equation.indexOf(equation[i+1]);
+}
+
+
+for (let i = 0; i < buttons.length; i++) {
+  const button = buttons[i];
+  button.addEventListener("click", function() {
+    if (button.textContent === 'C/AC' ) {
+      equation.value = '';
+      return;
+    } else {
+      if (button.textContent === '=' && equation.textContent.indexOf('+') > -1) {
+        equationArray = equation.value.split('+');
+        printAnswer( add(equationArray[0], equationArray[1]) );
+        return;
+      } else if (button.textContent === '=' && equation.textContent.indexOf('-') > -1) {
+        equationArray = equation.value.split('-');
+        printAnswer( subtract(equationArray[0], equationArray[1]) );
+        return;
+      } else {
+        equation.textContent += button.textContent;
+        return;
+      }
+    }
+  });
+}
+
+function calculator(equation) {
+  for (i = 0; i < equation.length; i++) {
+
+    let previous = equation.indexOf(equation[i-1]);
+    let current = equation.indexOf(equation[i]);
+    let next = equation.indexOf(equation[i+1]);
+
+    if ( equation[i] === 'x' ) {
+      if ( previous === -1 ) {
+        product *= parseInt(equation[i+1]);
+        equation.splice(current, 2);
+        calculate(equation);
+      } else {
+        let answer = multiply(equation[i-1], equation[i+1]);
+        product *= answer;
+        equation.splice(previous, 3);
+        calculate(equation);
+      }
+
+    } else if ( equation[i] === '+' ) {
+      if ( previous === -1 ) {
+        sum += parseInt(equation[i+1]);
+        equation.splice(current, 2);
+        calculate(equation);
+      } else {
+        let answer = add(equation[i-1], equation[i+1]);
+        sum += answer;
+        equation.splice(previous, 3);
+        calculate(equation);
+      }
+
+    } else if (equation[i] === '-') {
+      if ( previous === -1 ) {
+        difference -= parseInt(equation[i+1]);
+        equation.splice(current, 2);
+        calculate(equation);
+      } else {
+        let answer = subtract(equation[i-1], equation[i+1]);
+        difference += answer;
+        equation.splice(previous, 3);
+        calculate(equation);
+      }
+    }
+  }
+  let solution = (product / quotient) + sum + difference;
+  return solution;
+}
