9bf40b659890197a718580c5b59796978eeb4574
flan the dev <flanthedev@gmail.com>
2017-08-11 21:10:03 +0800

    divide function

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
 const calc = {
-  // sum: 0,
-  // difference: 0,
-  // product: 1,
-  // quotient: 1,
-  //
-  // reset: () => {
-  //   calc.sum = 0;
-  //   calc.different = 0;
-  //   calc.product = 1;
-  //   calc.quotient = 1;
-  // },
-
   run: (array) => {
-    // calc.reset();
     calc.multiply(array);
+    calc.divide(array);
     console.log(array);
   },
 
   multiply: (array) => {
-    for (i = 0; i < array.length; i++) {
-      if ( array[i] == 'x' && previous != -1 ) {
+    for ( let i = 0; i < array.length; i++ ) {
+      if ( array[i] === 'x') {
         let answer = parseInt(array[i-1]) * parseInt(array[i+1]);
         array.splice( array.indexOf(array[i-1]), 3, answer );
         console.log(array);
-        if (array.length) calc.multiply(array);
+        if ( array.length ) calc.multiply(array);
       }
     }
   },
+
+  divide: (array) => {
+    for ( let i = 0; i < array.length; i++ ) {
+      if ( array[i] === '/' ) {
+        let answer = parseInt(array[i-1]) / parseInt(array[i+1]);
+        array.splice( array.indexOf( array[i-1]), 3, answer );
+        console.log(array);
+        if ( array.length ) calc.divide(array);
+      }
+    }
+  }
 
 }
